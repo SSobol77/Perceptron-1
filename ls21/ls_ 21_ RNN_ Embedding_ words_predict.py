66b5d0f640a575bf80f3ec4054f37359b349809c
Siergej Sobolewski <108773983+SSobol77@users.noreply.github.com>
2023-08-19 03:11:50 +0200

    Create ls_ 21_ RNN_ Embedding_ words_predict.py

---
+++ ls21/ls_ 21_ RNN_ Embedding_ words_predict.py
@@ -0,0 +1,63 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+
+from tensorflow.keras.layers import Dense, SimpleRNN, Input, Embedding
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from tensorflow.keras.utils import to_categorical
+
+# Read and preprocess the text data
+with open('text', 'r', encoding='utf-8') as f:
+    texts = f.read()
+    texts = texts.replace('\ufeff', '')  # Remove the first invisible character
+
+maxWordsCount = 1000
+# Tokenize and preprocess the text
+tokenizer = Tokenizer(num_words=maxWordsCount, filters='!–"—#$%&amp;()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r«»',
+                      lower=True, split=' ', char_level=False)
+tokenizer.fit_on_texts([texts])
+
+dist = list(tokenizer.word_counts.items())
+print(dist[:10])
+
+data = tokenizer.texts_to_sequences([texts])
+res = np.array(data[0])
+
+inp_words = 3
+n = res.shape[0] - inp_words
+
+X = np.array([res[i:i + inp_words] for i in range(n)])
+Y = to_categorical(res[inp_words:], num_classes=maxWordsCount)
+
+# Build and compile the RNN model
+model = Sequential()
+model.add(Embedding(maxWordsCount, 256, input_length=inp_words))
+model.add(SimpleRNN(128, activation='tanh'))
+model.add(Dense(maxWordsCount, activation='softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+history = model.fit(X, Y, batch_size=32, epochs=50)
+
+# Function to generate phrases using the trained model
+def buildPhrase(texts, str_len=20):
+    res = texts
+    data = tokenizer.texts_to_sequences([texts])[0]
+    for i in range(str_len):
+        x = data[i: i + inp_words]
+        inp = np.expand_dims(x, axis=0)
+
+        pred = model.predict(inp)
+        indx = pred.argmax(axis=1)[0]
+        data.append(indx)
+
+        res += " " + tokenizer.index_word[indx]  # Append the predicted word to the string
+
+    return res
+
+# Generate a phrase using the model
+res = buildPhrase("позитив добавляет годы")
+print(res)
